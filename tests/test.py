d91ba4a321f87194b40720a3d7e32b0491eeffc4
LaCreme333 <115509261+LaCreme333@users.noreply.github.com>
2022-10-11 10:07:09 -0300

    0.2

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,8 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("I will speak this text")
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+engine.say("Bom dia Senhor")
 engine.runAndWait()
